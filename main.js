4d05876a9deac45456df09849157d41c6578c2d1
SaurabhKumar3 <saurabhsahani546@gmail.com>
2021-11-14 18:32:03 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+
+const path = require('path');
+const mongoose= require('mongoose')
+const session = require('express-session')
+
+const app = express();
+
+//dotenv.config( { path : '.env'} )
+const PORT = process.env.PORT || 3000
+
+mongoose.connect(url="mongodb://localhost:27017/node_crud",{useNewURLParser:true})
+const db = mongoose.connection;
+
+db.on('error',(error)=> console.log(error));
+db.once('open',()=> console.log("connected to the database"))
+// log requests
+
+
+app.use(express.urlencoded({extended:false}));
+app.use(express.json())
+
+app.use(session({
+    secret:'my secret key',
+    saveUninitialized:true,
+    resave:true
+}))
+app.use((req,res,next)=>{
+    res.locals.message= req.session.message
+    delete req.session.message;
+    next();
+});
+app.set('view engine','ejs');
+
+app.use("",require("./routes/routes"));
+app.use(express.static("uploads"));
+
+app.listen(PORT, ()=> { console.log(`Server is running on ${PORT}`)});
